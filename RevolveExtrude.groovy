072deea8eb7e39e0cab148817bd46868302b6be5
Kevin Harrington <mad.hephaestus@gmail.com>
2017-11-09 07:36:41 -0500

    comments

--- RevolveExtrude.groovy
+++ RevolveExtrude.groovy
@@ -1,8 +1,8 @@
 double wheelRadius = 33.0
 wheelHeight=17.6
 double radius = 6.0
-CSG profile = new RoundedCube(radius,// Y dimention
-					wheelRadius,// X dimention
+CSG profile = new RoundedCube(radius,// x dimention
+					wheelRadius,// y dimention
 				
 				wheelHeight//  Z dimention
 				)
@@ -10,12 +10,12 @@ CSG profile = new RoundedCube(radius,// Y dimention
 				.toCSG()// converts it to a CSG tor display
 				.toYMin()
 				.toZMin()
-				.movey(10)
+				
 //create a Cylinder
 height = 22.5
 CSG wheel_body = CSG.unionAll(
 		Extrude.revolve(profile,
-		(double)0, // rotation center radius, if 0 it is a circle, larger is a donut. Note it can be negative too
+		(double)10, // rotation center radius, if 0 it is a circle, larger is a donut. Note it can be negative too
 		(double)180,// degrees through wich it should sweep
 		(int)10)//number of sweep increments
 		)
